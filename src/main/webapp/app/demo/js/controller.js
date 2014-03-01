fb85528b93a8ec5a20bfc269e5945a73904a9bc6
Tobias Koltsch <tobias@koltsch.de>
2014-03-01 10:37:36 +0100

    full angular demo and initial state of example

---
+++ src/main/webapp/app/demo/js/controller.js
@@ -0,0 +1,33 @@
+"use strict";
+
+function groceryListCtrl($scope) {
+
+    $scope.groceries = [
+        {name: "noodles", quantity: 2, price: 0.99},
+        {name: "juice", quantity: 4, price: 1.99},
+        {name: "milk", quantity: 3, price: 0.49},
+        {name: "cereal", quantity: 1, price: 2.99}
+    ];
+
+    $scope.grocery = {};
+
+    $scope.addGrocery = function() {
+        $scope.groceries.push($scope.grocery);
+        $scope.grocery = {};
+    };
+
+    $scope.removeGrocery = function(index) {
+        $scope.groceries.splice(index, 1);
+    };
+
+    $scope.total = function() {
+        var total = 0;
+        for (var i=0; i < $scope.groceries.length; i++) {
+            total = total + ($scope.groceries[i].quantity * $scope.groceries[i].price);
+        }
+
+        return total;
+    };
+}
+
+
