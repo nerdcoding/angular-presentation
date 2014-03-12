2cae78f43620dc5a6d152d284bbcfeefcd4d8b57
Tobias Koltsch <tobias@koltsch.de>
2014-03-12 19:48:03 +0100

    finished presentation example

--- src/main/webapp/app/js/controller.js
+++ src/main/webapp/app/js/controller.js
@@ -1,2 +1,44 @@
 'use strict'
 
+
+function groceryListCtrl($scope, $http) {
+
+    $scope.groceries = [];
+    $http.get('/angular/rest/grocery').success(function(data) {
+        $scope.groceries = data;
+    });
+
+
+//    $scope.groceries = [
+//        {name: "noodle", quantity: 2, price: 0.99},
+//        {name: "juice", quantity: 4, price: 1.99},
+//        {name: "milk", quantity: 3, price: 0.49},
+//        {name: "cereal", quantity: 1, price: 2.99}
+//    ];
+
+    $scope.removeGrocery = function(index) {
+        $scope.groceries.splice(index, 1);
+    }
+
+    $scope.total = function() {
+        var total = 0;
+        for (var i=0; i < $scope.filteredGroceries.length; i++ ) {
+            total = total + ($scope.filteredGroceries[i].quantity * $scope.filteredGroceries[i].price);
+        }
+        return total;
+    }
+
+    $scope.addGrocery = function() {
+        $http.post('/angular/rest/grocery', $scope.grocery).success(function() {
+            $http.get('/angular/rest/grocery').success(function(data) {
+                $scope.groceries = data;
+                $scope.grocery = {};
+            });
+        });
+
+        //$scope.groceries.push($scope.grocery);
+    }
+
+}
+
+
